c7a26bf8fe08b36274ef9b33f6390f65bbd15bed
Sensational <113780798+sensationalspace@users.noreply.github.com>
2024-06-10 08:17:58 +0100

    Update main.tf
    removed previous lab experiment

--- main.tf
+++ main.tf
@@ -85,16 +85,7 @@ resource "azurerm_network_interface" "nic" {
   }
 }
 
-resource "azurerm_storage_account" "examplestorageacct" {
-  name                = "examplestorageacct"
-  resource_group_name = azurerm_resource_group.rg.name
 
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  access_tier              = "Hot"
-  account_replication_type = "LRS"
-  account_kind             = "StorageV2"
-}
 # Virtual machine definition
 resource "azurerm_virtual_machine" "vm" {
   name                  = "${var.prefix}-vm"
