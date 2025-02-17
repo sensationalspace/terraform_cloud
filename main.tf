7dff3a2dce67f7c765d0dc9e72f84dbf8f8a3a0b
rategainvishal <vishaldeep109@gmail.com>
2025-02-17 19:37:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -89,23 +89,23 @@ resource "azurerm_network_interface" "nic" {
 # Blob Storage definition
 
 resource "azurerm_storage_account" "blob_storage" {
-  name                     = "vishal-blob-storage"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
+  name                     = "${var.prefix}-vishal-storage"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
 resource "azurerm_storage_container" "blob_storage" {
   name                  = "content"
-  storage_account_name  = azurerm_storage_account.example.name
+  storage_account_name  = "${var.prefix}-vishal-storage"
   container_access_type = "private"
 }
 
 resource "azurerm_storage_blob" "blob_storage" {
-  name                   = "my-awesome-content.zip"
-  storage_account_name   = azurerm_storage_account.example.name
-  storage_container_name = azurerm_storage_container.example.name
+  name                   = "${var.prefix}-vishal-blob-storage"
+  storage_account_name   = "${var.prefix}-vishal-storage"
+  storage_container_name = content
   type                   = "Block"
   source                 = "some-local-file.zip"
 }
