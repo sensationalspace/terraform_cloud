2027cf78d56a6e2b0b6ed98a84e0905ec5edf1c4
Harsh Verma <harsh.verma@celebaltech.com>
2025-05-29 15:24:40 +0530

    CHANGE

--- main.tf
+++ main.tf
@@ -1,184 +1,184 @@
-#######################################
-#            TERRAFORM BLOCK          #
-#######################################
-terraform {
-  required_version = ">= 0.14.9"
+# #######################################
+# #            TERRAFORM BLOCK          #
+# #######################################
+# terraform {
+#   required_version = ">= 0.14.9"
 
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">= 4.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = ">= 3.5.1"
-    }
-  }
-}
+#   required_providers {
+#     azurerm = {
+#       source  = "hashicorp/azurerm"
+#       version = ">= 4.0"
+#     }
+#     random = {
+#       source  = "hashicorp/random"
+#       version = ">= 3.5.1"
+#     }
+#   }
+# }
 
-#######################################
-#            PROVIDER BLOCK           #
-#######################################
-provider "azurerm" {
-  features {}
+# #######################################
+# #            PROVIDER BLOCK           #
+# #######################################
+# provider "azurerm" {
+#   features {}
 
-  subscription_id = var.subscription_id
-  tenant_id       = var.tenant_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
+#   subscription_id = var.subscription_id
+#   tenant_id       = var.tenant_id
+#   client_id       = var.client_id
+#   client_secret   = var.client_secret
 
-  resource_provider_registrations = "none"
-}
+#   resource_provider_registrations = "none"
+# }
 
-#######################################
-#              VARIABLES              #
-#######################################
-variable "prefix" {
-  type    = string
-  default = "terra"
-}
+# #######################################
+# #              VARIABLES              #
+# #######################################
+# variable "prefix" {
+#   type    = string
+#   default = "terra"
+# }
 
-variable "client_id" {
-  type = string
-}
+# variable "client_id" {
+#   type = string
+# }
 
-variable "client_secret" {
-  type      = string
-  sensitive = true
-}
+# variable "client_secret" {
+#   type      = string
+#   sensitive = true
+# }
 
-variable "subscription_id" {
-  type = string
-}
+# variable "subscription_id" {
+#   type = string
+# }
 
-variable "tenant_id" {
-  type = string
-}
+# variable "tenant_id" {
+#   type = string
+# }
 
-#######################################
-#         RESOURCE GROUP              #
-#######################################
-resource "azurerm_resource_group" "rg" {
-  name     = "${var.prefix}-resourcegroup"
-  location = "Central India"
-}
+# #######################################
+# #         RESOURCE GROUP              #
+# #######################################
+# resource "azurerm_resource_group" "rg" {
+#   name     = "${var.prefix}-resourcegroup"
+#   location = "Central India"
+# }
 
-#######################################
-#             NETWORKING              #
-#######################################
-resource "azurerm_virtual_network" "vnet" {
-  name                = "${var.prefix}-vnet"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-}
+# #######################################
+# #             NETWORKING              #
+# #######################################
+# resource "azurerm_virtual_network" "vnet" {
+#   name                = "${var.prefix}-vnet"
+#   address_space       = ["10.0.0.0/16"]
+#   location            = azurerm_resource_group.rg.location
+#   resource_group_name = azurerm_resource_group.rg.name
+# }
 
-resource "azurerm_subnet" "internal" {
-  name                 = "${var.prefix}-internal"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.2.0/24"]
-}
-resource "azurerm_subnet" "pe" {
-  name                              = "${var.prefix}-pe"
-  resource_group_name               = azurerm_resource_group.rg.name
-  virtual_network_name              = azurerm_virtual_network.vnet.name
-  address_prefixes                  = ["10.0.3.0/24"]
-  private_endpoint_network_policies = "Enabled"
-}
+# resource "azurerm_subnet" "internal" {
+#   name                 = "${var.prefix}-internal"
+#   resource_group_name  = azurerm_resource_group.rg.name
+#   virtual_network_name = azurerm_virtual_network.vnet.name
+#   address_prefixes     = ["10.0.2.0/24"]
+# }
+# resource "azurerm_subnet" "pe" {
+#   name                              = "${var.prefix}-pe"
+#   resource_group_name               = azurerm_resource_group.rg.name
+#   virtual_network_name              = azurerm_virtual_network.vnet.name
+#   address_prefixes                  = ["10.0.3.0/24"]
+#   private_endpoint_network_policies = "Enabled"
+# }
 
-resource "azurerm_network_interface" "nic" {
-  name                = "${var.prefix}-nic"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+# resource "azurerm_network_interface" "nic" {
+#   name                = "${var.prefix}-nic"
+#   location            = azurerm_resource_group.rg.location
+#   resource_group_name = azurerm_resource_group.rg.name
 
-  ip_configuration {
-    name                          = "primary"
-    subnet_id                     = azurerm_subnet.internal.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
+#   ip_configuration {
+#     name                          = "primary"
+#     subnet_id                     = azurerm_subnet.internal.id
+#     private_ip_address_allocation = "Dynamic"
+#   }
+# }
 
-#######################################
-#          VIRTUAL MACHINE            #
-#######################################
-resource "azurerm_virtual_machine" "vm" {
-  name                  = "${var.prefix}-vm"
-  location              = azurerm_resource_group.rg.location
-  resource_group_name   = azurerm_resource_group.rg.name
-  network_interface_ids = [azurerm_network_interface.nic.id]
-  vm_size               = "Standard_DS1_v2"
+# #######################################
+# #          VIRTUAL MACHINE            #
+# #######################################
+# resource "azurerm_virtual_machine" "vm" {
+#   name                  = "${var.prefix}-vm"
+#   location              = azurerm_resource_group.rg.location
+#   resource_group_name   = azurerm_resource_group.rg.name
+#   network_interface_ids = [azurerm_network_interface.nic.id]
+#   vm_size               = "Standard_DS1_v2"
 
-  storage_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-focal"
-    sku       = "20_04-lts"
-    version   = "latest"
-  }
+#   storage_image_reference {
+#     publisher = "Canonical"
+#     offer     = "0001-com-ubuntu-server-focal"
+#     sku       = "20_04-lts"
+#     version   = "latest"
+#   }
 
-  storage_os_disk {
-    name              = "${var.prefix}-osdisk"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
+#   storage_os_disk {
+#     name              = "${var.prefix}-osdisk"
+#     caching           = "ReadWrite"
+#     create_option     = "FromImage"
+#     managed_disk_type = "Standard_LRS"
+#   }
 
-  os_profile {
-    computer_name  = "hostname"
-    admin_username = "tfadmin"
-    admin_password = "Password1234!"
-  }
+#   os_profile {
+#     computer_name  = "hostname"
+#     admin_username = "tfadmin"
+#     admin_password = "Password1234!"
+#   }
 
-  os_profile_linux_config {
-    disable_password_authentication = false
-  }
+#   os_profile_linux_config {
+#     disable_password_authentication = false
+#   }
 
-  tags = {
-    environment = "staging"
-  }
-}
+#   tags = {
+#     environment = "staging"
+#   }
+# }
 
-resource "azurerm_storage_account" "this" {
-  name                     = "${var.prefix}saeftgbtgsd"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+# resource "azurerm_storage_account" "this" {
+#   name                     = "${var.prefix}saeftgbtgsd"
+#   resource_group_name      = azurerm_resource_group.rg.name
+#   location                 = azurerm_resource_group.rg.location
+#   account_tier             = "Standard"
+#   account_replication_type = "LRS"
 
-  tags = {
-    environment = "staging"
-  }
-}
+#   tags = {
+#     environment = "staging"
+#   }
+# }
 
-resource "azurerm_private_endpoint" "storage_account" {
-  name                = "${var.prefix}-pe-sa"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  subnet_id           = azurerm_subnet.pe.id
+# resource "azurerm_private_endpoint" "storage_account" {
+#   name                = "${var.prefix}-pe-sa"
+#   location            = azurerm_resource_group.rg.location
+#   resource_group_name = azurerm_resource_group.rg.name
+#   subnet_id           = azurerm_subnet.pe.id
 
-  private_service_connection {
-    name                           = "${var.prefix}-pe-sa"
-    private_connection_resource_id = azurerm_storage_account.this.id
-    subresource_names              = ["blob"]
-    is_manual_connection           = false
-  }
+#   private_service_connection {
+#     name                           = "${var.prefix}-pe-sa"
+#     private_connection_resource_id = azurerm_storage_account.this.id
+#     subresource_names              = ["blob"]
+#     is_manual_connection           = false
+#   }
 
-  tags = {
-    environment = "staging"
-  }
-}
+#   tags = {
+#     environment = "staging"
+#   }
+# }
 
-resource "azurerm_private_dns_zone" "blob" {
-  name                = "privatelink.blob.core.windows.net"
-  resource_group_name = azurerm_resource_group.rg.name
-  tags = {
-    environment = "staging"
-  }
-}
+# resource "azurerm_private_dns_zone" "blob" {
+#   name                = "privatelink.blob.core.windows.net"
+#   resource_group_name = azurerm_resource_group.rg.name
+#   tags = {
+#     environment = "staging"
+#   }
+# }
 
-resource "azurerm_private_dns_zone_virtual_network_link" "blob" {
-  name                  = "${azurerm_private_dns_zone.blob.name}-${azurerm_virtual_network.vnet.name}"
-  resource_group_name   = azurerm_resource_group.rg.name
-  private_dns_zone_name = azurerm_private_dns_zone.blob.name
-  virtual_network_id    = azurerm_virtual_network.vnet.id
-}
+# resource "azurerm_private_dns_zone_virtual_network_link" "blob" {
+#   name                  = "${azurerm_private_dns_zone.blob.name}-${azurerm_virtual_network.vnet.name}"
+#   resource_group_name   = azurerm_resource_group.rg.name
+#   private_dns_zone_name = azurerm_private_dns_zone.blob.name
+#   virtual_network_id    = azurerm_virtual_network.vnet.id
+# }
